6840bfe2d4594ec84c4df3d3edbb987b0351f2b5
ryanliwendi <liryan@usc.edu>
2025-04-18 11:40:59 -0700

    Added wordle.cpp

--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+set<string> set_combine(set<string> s1, set<string> s2);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,37 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+    set<string> words;
+    if (in.find('-') == string::npos)
+    {
+        if (floating.empty() && dict.find(in) != dict.end())
+            words.insert(in);
+        return words;
+    }
+    int pos = in.find('-');
+    string nextin = in;
+    for (int i = 0; i < floating.size(); i++)
+    {
+        nextin[pos] = floating[i];
+        string nextfloat = floating.substr(0, i) + floating.substr(i + 1);
+        set<string> temp = wordle(nextin, nextfloat, dict);
+        words = set_combine(words, temp);
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        char c = 'a' + i;
+        nextin[pos] = c;
+        set<string> temp = wordle(nextin, floating, dict);
+        words = set_combine(words, temp);
+    }
+    return words;
 }
 
 // Define any helper functions here
+set<string> set_combine(set<string> s1, set<string> s2)
+{
+    set<string> ret = s1;
+    for (set<string>::iterator it = s2.begin(); it != s2.end(); ++it)
+        ret.insert(*it);
+    return ret;
+}
