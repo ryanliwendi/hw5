16dbe20d1868df70d536719897063bd51515eb24
ryanliwendi <liryan@usc.edu>
2025-04-21 15:17:37 -0700

    Updated schedwork.cpp

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,18 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool backtrack(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& curShifts,
+    int curDay);
 
+void all_combinations(const vector<bool> daily_avail,
+    int dailyNeed,
+    vector<vector<Worker_T>>& all_combos,
+    vector<Worker_T>& cur_schedule,
+    int start);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -37,9 +48,67 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    int numWorkers = avail[0].size();
+    vector<int> curShifts(numWorkers, 0);
+    return backtrack(avail, dailyNeed, maxShifts, sched, curShifts, 0);
+}
 
+bool backtrack(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+    vector<int>& curShifts,
+    int curDay)
+{
+    if (curDay == avail.size())
+        return true;
+    vector<vector<Worker_T>> all_combos;
+    vector<bool> daily_avail = avail[curDay];
+    vector<Worker_T> cur_schedule;
+    all_combinations(daily_avail, dailyNeed, all_combos, cur_schedule, 0);
+    for (int i = 0; i < all_combos.size(); i++)
+    {
+        vector<Worker_T> combo = all_combos[i];
+        bool success = true;
+        for (Worker_T j : combo)
+        {
+            curShifts[j]++;
+            if (curShifts[j] > maxShifts)
+                success = false;
+        }
+        if (success)
+        {
+            sched.push_back(combo);
+            bool temp = backtrack(avail, dailyNeed, maxShifts, sched, curShifts, curDay + 1);
+            if (temp)
+                return true;
+            sched.pop_back();
+        }
+        for (Worker_T j : combo)
+            curShifts[j]--;
+    }
+    return false;
+}
 
-
-
+void all_combinations(const vector<bool> daily_avail,
+    int dailyNeed,
+    vector<vector<Worker_T>>& all_combos,
+    vector<Worker_T>& cur_schedule,
+    int start)
+{
+    if (cur_schedule.size() == dailyNeed)
+    {
+        all_combos.push_back(cur_schedule);
+        return;
+    }
+    for (int i = start; i < daily_avail.size(); i++)
+    {
+        if (daily_avail[i] == true)
+        {
+            cur_schedule.push_back(i);
+            all_combinations(daily_avail, dailyNeed, all_combos, cur_schedule, i+1);
+            cur_schedule.pop_back();
+        }
+    }
 }
 
